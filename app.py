a2eef551fc4957a4addc4ce48b8fa91879366703
MuhammadZumair <zumairmuhammad@gmail.com>
2017-09-23 19:57:23 +0500

    added database config

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from chalice import Chalice, Response
+from chalice import Chalice
+import pyrebase
 
-app = Chalice(app_name='hello world')
+config = {
+    "apiKey": "AIzaSyDpTe7Ijr0iHmW6n26c71kQaGkYBY6ZiRk",
+    "authDomain": "imanager-ai.firebaseapp.com",
+    "databaseURL": "https://imanager-ai.firebaseio.com",
+    "projectId": "imanager-ai",
+    "storageBucket": "imanager-ai.appspot.com",
+    "messagingSenderId": "255523144424"
+}
 
+firebase = pyrebase.initialize_app(config)
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+app = Chalice(app_name='calculator')
 
 
 @app.route('/webhook', methods=['POST', 'PUT'])
 def index():
-    request = app.current_request
-    num1 = int(request.json_body.get("result").get("parameters").get("number"))
-    num2 = int(request.json_body.get("result").get("parameters").get("number1"))
-    op = request.json_body.get("result").get("parameters").get("operation")
+    req = app.current_request
+    print("req: ", req)
+    num1 = int(req.json_body.get("result").get("parameters").get("number"))
+    num2 = int(req.json_body.get("result").get("parameters").get("number1"))
+    op = req.json_body.get("result").get("parameters").get("operation")
     result = calculation(num1, num2, op)
+    print("result: ", result)
+    db = firebase.database()
+    data = {"name": "Mortimer 'Morty' Smith"}
+    db.child("users").push(data)
     return {
-                'speech': result
-            }
+        'speech': result
+    }
+
+
 def calculation(num1, num2, op):
     if(op == "sub"):
         return num1 - num2
